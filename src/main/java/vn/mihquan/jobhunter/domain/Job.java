5f54a813bf6dbf7b0e6436a468991b4e51eb8733
mihquan <mihquannguyen47@gmail.com>
2025-09-30 03:45:27 +0700

    Create model Resume

--- src/main/java/vn/mihquan/jobhunter/domain/Job.java
+++ src/main/java/vn/mihquan/jobhunter/domain/Job.java
@@ -3,6 +3,7 @@ package vn.mihquan.jobhunter.domain;
 import java.time.Instant;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
@@ -17,6 +18,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -67,6 +69,10 @@ public class Job {
     @JsonIgnoreProperties(value = { "jobs" })
     @JoinTable(name = "job_skill", joinColumns = @JoinColumn(name = "job_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
     private List<Skill> skills;
+
+    @OneToMany(mappedBy = "job", fetch = FetchType.LAZY)
+    @JsonIgnore
+    List<Resume> resumes;
 
     @PrePersist
     public void handleBeforeCreate() {
---
+++ src/main/java/vn/mihquan/jobhunter/util/constant/ResumeStateEnum.java
@@ -0,0 +1,5 @@
+package vn.mihquan.jobhunter.util.constant;
+
+public enum ResumeStateEnum {
+    PENDING, REVIEWING, APPROVED, REJECTED
+}
