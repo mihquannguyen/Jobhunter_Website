c1cd0f212f55d7eb1f0228c748c98b3fa6b13818
mihquan <mihquannguyen47@gmail.com>
2025-09-30 03:18:13 +0700

    upload file

---
+++ src/main/java/vn/mihquan/jobhunter/controller/FileController.java
@@ -0,0 +1,59 @@
+package vn.mihquan.jobhunter.controller;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import vn.mihquan.jobhunter.domain.response.file.ResUploadFileDTO;
+import vn.mihquan.jobhunter.service.FileService;
+import vn.mihquan.jobhunter.util.annotation.ApiMessage;
+import vn.mihquan.jobhunter.util.error.StorageException;
+
+@RestController
+@RequestMapping("/api/v1")
+public class FileController {
+    @Value("${hoidanit.upload-file.base-uri}")
+    private String baseUri;
+
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/files")
+    @ApiMessage("Upload single file")
+    public ResponseEntity<ResUploadFileDTO> upload(
+            @RequestParam(name = "file", required = false) MultipartFile file,
+            @RequestParam("folder") String folder) throws URISyntaxException, IOException, StorageException {
+        // skip validate
+        if (file.isEmpty()) {
+            throw new StorageException("File is empty. Please upload a file.");
+        }
+        String fileName = file.getOriginalFilename();
+        List<String> allowedExtensions = Arrays.asList("pdf", "jpg", "jpeg", "png", "doc", "docx");
+        boolean isValid = allowedExtensions.stream().anyMatch(item -> fileName.toLowerCase().endsWith(item));
+
+        if (!isValid) {
+            throw new StorageException("Invalid file extension. only allow " + allowedExtensions.toString());
+        }
+        // create a directory if no exist
+        this.fileService.createDirectory(baseUri + folder);
+        // store file
+        String uploadFile = this.fileService.store(file, folder);
+
+        ResUploadFileDTO res = new ResUploadFileDTO(uploadFile, Instant.now());
+
+        return ResponseEntity.ok().body(res);
+    }
+}
