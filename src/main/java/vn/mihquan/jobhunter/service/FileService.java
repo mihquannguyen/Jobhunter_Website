7e0692c570c7b0dc3e92622abf80f0e904480eda
mihquan <mihquannguyen47@gmail.com>
2025-09-30 03:33:36 +0700

    download a file

--- src/main/java/vn/mihquan/jobhunter/service/FileService.java
+++ src/main/java/vn/mihquan/jobhunter/service/FileService.java
@@ -1,6 +1,8 @@
 package vn.mihquan.jobhunter.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -11,13 +13,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileService {
     @Value("${hoidanit.upload-file.base-uri}")
-    private String baseUri;
+    private String baseURI;
 
     public void createDirectory(String folder) throws URISyntaxException {
         URI uri = new URI(folder);
@@ -39,12 +42,33 @@ public class FileService {
             IOException {
         // create unique filename
         String finalName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
-        URI uri = new URI(baseUri + folder + "/" + finalName);
+        URI uri = new URI(baseURI + folder + "/" + finalName);
         Path path = Paths.get(uri);
         try (InputStream inputStream = file.getInputStream()) {
             Files.copy(inputStream, path,
                     StandardCopyOption.REPLACE_EXISTING);
         }
         return finalName;
+    }
+
+    public long getFileLength(String fileName, String folder) throws URISyntaxException {
+        URI uri = new URI(baseURI + folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File tmpDir = new File(path.toString());
+
+        // file không tồn tại, hoặc file là 1 director => return 0
+        if (!tmpDir.exists() || tmpDir.isDirectory())
+            return 0;
+        return tmpDir.length();
+    }
+
+    public InputStreamResource getResource(String fileName, String folder)
+            throws URISyntaxException, FileNotFoundException {
+        URI uri = new URI(baseURI + folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File file = new File(path.toString());
+        return new InputStreamResource(new FileInputStream(file));
     }
 }
