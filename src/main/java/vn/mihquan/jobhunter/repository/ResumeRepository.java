9c8f6b1b9a48c0821488d713e82b9d3096b29959
mihquan <mihquannguyen47@gmail.com>
2025-09-30 15:53:10 +0700

    Create CRUD resume

---
+++ src/main/java/vn/mihquan/jobhunter/repository/ResumeRepository.java
@@ -0,0 +1,13 @@
+package vn.mihquan.jobhunter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import vn.mihquan.jobhunter.domain.Resume;
+
+@Repository
+public interface ResumeRepository extends JpaRepository<Resume, Long>,
+        JpaSpecificationExecutor<Resume> {
+
+}
