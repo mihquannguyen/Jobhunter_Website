b4c4c677de294387c1e91fb73e80a29041f9d12b
mihquan <mihquannguyen47@gmail.com>
2025-10-08 03:09:36 +0700

    send email

--- src/main/java/vn/mihquan/jobhunter/controller/EmailController.java
+++ src/main/java/vn/mihquan/jobhunter/controller/EmailController.java
@@ -4,14 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.mihquan.jobhunter.service.EmailService;
+import vn.mihquan.jobhunter.service.SubscriberService;
 import vn.mihquan.jobhunter.util.annotation.ApiMessage;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailController {
+
+    private final EmailService emailService;
+    private final SubscriberService subscriberService;
+
+    public EmailController(EmailService emailService,
+            SubscriberService subscriberService) {
+        this.emailService = emailService;
+        this.subscriberService = subscriberService;
+    }
+
     @GetMapping("/email")
-    @ApiMessage("send simple email")
+    @ApiMessage("Send simple email")
+    // @Scheduled(cron = "*/30 * * * * *")
+    // @Transactional
     public String sendSimpleEmail() {
+        // this.emailService.sendSimpleEmail();
+        // this.emailService.sendEmailSync("ads.hoidanit@gmail.com", "test send email",
+        // "<h1> <b> hello </b> </h1>", false,
+        // true);
+        // this.emailService.sendEmailFromTemplateSync("ads.hoidanit@gmail.com", "test
+        // send email", "job");
+        this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
     }
 }
